285f1621aef04c13cfaf5d9e3ac001c996df7d01
Janusz-Domaradzki <62700201+Janusz-Domaradzki@users.noreply.github.com>
2021-03-21 00:14:45 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -13,159 +13,40 @@ struct Zdarzenie
 	int p_j;
 	int q_j;
 	int indx;
-
-
 };
 
-void Wyswietl(vector<Zdarzenie> z1,int wlk)
-{
-	cout << "j = [ ";
-	for(int i=0; i<wlk; i++)
-	{
-		cout << z1[i].indx << " ";
-	}
-	cout << "]" << endl;
+//Funkcja wyswietlajaca zawartosc wektora z zadaniami
+void Wyswietl(vector<Zdarzenie> z1);
 
-	cout << "p = [ ";
-	for(int i=0; i<wlk; i++)
-	{
-		cout << z1[i].p_j << " ";
-	}
-	cout << "]" << endl;
+//Funkcja znajdujaca Zdarzenie z najmniejszym parametrem r
+int min_r(vector<Zdarzenie> z1);
 
-	cout << "r = [ ";
-	for(int i=0; i<wlk; i++)
-	{
-		cout << z1[i].r_j << " ";
-	}
-	cout << "]" << endl;
+//Funkcja znajdujaca indeks zdarzenia o najmniejszym parametrze r
+int min_r_idx(vector<Zdarzenie> z1);
 
-	cout << "q = [ ";
-	for(int i=0; i<wlk; i++)
-	{
-		cout << z1[i].q_j << " ";
-	}
-	cout << "]" << endl;
-}
+//Funkcja znajdujaca indeks zdarzenia o najwiekszym parametrze q
+int max_q_idx(vector<Zdarzenie> z1);
 
-int minimum(vector<Zdarzenie> z1, int wlk)
-{
-	int mm=z1[0].r_j;
-	int id;
-	for(int i=1; i<wlk; i++)
-	{
+//Algorytm Schrage
+vector<Zdarzenie> Schrage_alg1(vector<Zdarzenie> z1);
 
-		if(z1[i].r_j<mm)
-		{
-			mm=z1[i].r_j;
-			id=i;
-		}
-	}
-	//cout << "r: " << mm << endl;
-	return mm;
-}
-
-int minimum_idx(vector<Zdarzenie> z1, int wlk)
-{
-	int mm=z1[0].r_j;
-	int id = 0;
-	for(int i=1; i<wlk; i++)
-	{
-
-		if(z1[i].r_j<mm)
-		{
-			mm=z1[i].r_j;
-			id=i;
-		}
-	}
-	//cout << "id_min: " <<id << endl;
-	return id;
-}
-
-int maximum_idx(vector<Zdarzenie> z1, int wlk)
-{
-	int mm=z1[0].q_j;
-	int id = 0;
-	for(int i=1; i<wlk; i++)
-	{
-
-		if(z1[i].q_j>mm)
-		{
-			mm=z1[i].q_j;
-			id=i;
-		}
-	}
-	//cout << "id_max: " << id << endl;
-	return id;
-}
-
-vector<Zdarzenie> Schrage_alg1(vector<Zdarzenie> z1, int wlk)
-{
-	int k = 0;
-	int new_j;
-	vector <Zdarzenie> G;
-	vector <Zdarzenie> N;
-	vector <Zdarzenie> Pi;
-	N = z1;
-	int t = minimum(N,N.size());
-	cout << "C = [ ";
-	cout << t << " ";
-	
-	//cout << "??: " <<(int) N.begin() << endl;
-	//cout << G.size() << " " << N.size() << " " << t << endl;
-	//cout << G.capacity() << " " << N.capacity() << " " << t << endl; 
-
-	//int lag, bo nie wiem jak napisac funkcje sprawdzajaca pustosc tabeli
-
-	while ( G.size() != 0 || N.size() != 0)
-	{
-		//cout << "xd" << endl;
-		while(N.size() != 0 && minimum(N,N.size()) <=t)
-		{
-			new_j = minimum_idx(N,N.size());
-			G.push_back(N[new_j]);
-			N.erase(N.begin()+new_j);
-			//cout << N.size() << endl;
-			//Wyswietl(N,N.size());
-		}
-		if(G.size() != 0)
-		{
-			new_j=maximum_idx(G,G.size());
-			//cout <<"idx G: " << new_j << endl;
-			//cout << "G1: " << endl;
-			//Wyswietl(G,G.size());
-			Pi.push_back(G[new_j]);
-			//cout << endl;
-			G.erase(G.begin()+new_j);
-			//cout << "G2: " << endl;
-			//Wyswietl(G,G.size());
-			t=t+N[new_j].p_j;
-			k+=1;
-			//cout << "Pi =" << endl;
-			//Wyswietl(Pi,Pi.size());
-			//cout << endl;
-		}
-		else t = minimum(N,N.size());
-		cout << t << " ";
-	}
-	cout << "]";
-	return Pi;
-}
-
+//Kryterium optymalizacyjne, czyli czas zakonczenia wszystkich zadan i opuszczenia ich przez system
+void Kryt(vector<Zdarzenie> wynik);
 
 int main()
 {
 	srand(time(NULL));
 	int wlk;
 	int A=0;
-	int x;
-
+	int x, i;
+	
 	cout << "Wprowadz wielkosc instancji: ";
 	cin >> wlk;
 	Zdarzenie* quest = new Zdarzenie [wlk];
+	
 	vector<Zdarzenie> quest2;
 	vector<Zdarzenie> wynik;
-	for(int i=0; i<wlk;i++)
+	for(i=0; i<wlk;i++)
 	{
 		quest[i].indx = i+1;
 		quest[i].p_j=rand()%29+1;
@@ -187,21 +68,191 @@ int main()
 		x = 29;
 		break;
 	}
-	for(int i=0; i<wlk;i++)
+	for(i=0; i<wlk;i++)
 	{
 		quest[i].r_j=rand()%A+1;
 		quest[i].q_j=rand()%x+1;
 		
 	}
-	for(int i=0; i<wlk;i++)
+	for(i=0; i<wlk;i++)
 	{
 		quest2.push_back(quest[i]);
 	}
+	delete [] quest;
 	cout << "Przed algorytmem: " << endl;
-	Wyswietl(quest2,wlk);
-	wynik=Schrage_alg1(quest2,quest2.size());
+	Wyswietl(quest2);
+	Kryt(quest2);
+	wynik=Schrage_alg1(quest2);
 	cout << endl;
 	cout << "Po algorytmie Schrage 1: " << endl;
-	Wyswietl(wynik,wynik.size());
+	Wyswietl(wynik);
+
+	Kryt(wynik);
+
 	return 0;
+}
+
+
+
+
+void Wyswietl(vector<Zdarzenie> z1)
+{
+	cout << "j = [ ";
+	for(int i=0; i<z1.size(); i++)
+	{
+		cout << z1[i].indx << " ";
+	}
+	cout << "]" << endl;
+
+	cout << "p = [ ";
+	for(int i=0; i<z1.size(); i++)
+	{
+		cout << z1[i].p_j << " ";
+	}
+	cout << "]" << endl;
+
+	cout << "r = [ ";
+	for(int i=0; i<z1.size(); i++)
+	{
+		cout << z1[i].r_j << " ";
+	}
+	cout << "]" << endl;
+
+	cout << "q = [ ";
+	for(int i=0; i<z1.size(); i++)
+	{
+		cout << z1[i].q_j << " ";
+	}
+	cout << "]" << endl;
+}
+
+int min_r(vector<Zdarzenie> z1)
+{
+	int mm=z1[0].r_j;
+	int id = 0;
+	for(int i=1; i<z1.size(); i++)
+	{
+
+		if(z1[i].r_j<mm)
+		{
+			mm=z1[i].r_j;
+			id=i;
+		}
+	}
+	//cout << "r: " << mm << endl;
+	return mm;
+}
+
+int min_r_idx(vector<Zdarzenie> z1)
+{
+	int mm=z1[0].r_j;
+	int id = 0;
+	for(int i=1; i<z1.size(); i++)
+	{
+
+		if(z1[i].r_j<mm)
+		{
+			mm=z1[i].r_j;
+			id=i;
+		}
+	}
+	//cout << "id_min: " <<id << endl;
+	return id;
+}
+
+int max_q_idx(vector<Zdarzenie> z1)
+{
+	int mm=z1[0].q_j;
+	int id = 0;
+	for(int i=1; i<z1.size(); i++)
+	{
+
+		if(z1[i].q_j>mm)
+		{
+			mm=z1[i].q_j;
+			id=i;
+		}
+	}
+	//cout << "id_max: " << id << endl;
+	return id;
+}
+
+vector<Zdarzenie> Schrage_alg1(vector<Zdarzenie> z1)
+{
+	int k = 0;
+	int new_j;
+	vector <Zdarzenie> G;
+	vector <Zdarzenie> N;
+	vector <Zdarzenie> Pi;
+	N = z1;
+	int t = min_r(N);
+	cout << "t = [ ";
+	cout << t << " ";
+	
+	//cout << "??: " <<(int) N.begin() << endl;
+	//cout << G.size() << " " << N.size() << " " << t << endl;
+	//cout << G.capacity() << " " << N.capacity() << " " << t << endl; 
+
+	//int lag, bo nie wiem jak napisac funkcje sprawdzajaca pustosc tabeli
+
+	while ( G.size() != 0 || N.size() != 0)
+	{
+		//cout << "xd" << endl;
+		while(N.size() != 0 && min_r(N) <=t)
+		{
+			new_j = min_r_idx(N);
+			G.push_back(N[new_j]);
+			N.erase(N.begin()+new_j);
+			//cout << N.size() << endl;
+			//Wyswietl(N,N.size());
+		}
+		if(G.size() != 0)
+		{
+			new_j=max_q_idx(G);
+			//cout <<"idx G: " << new_j << endl;
+			//cout << "G1: " << endl;
+			//Wyswietl(G,G.size());
+			Pi.push_back(G[new_j]);
+			//cout << endl;
+			G.erase(G.begin()+new_j);
+			//cout << "G2: " << endl;
+			//Wyswietl(G,G.size());
+			t=t+N[new_j].p_j;
+			k+=1;
+			//cout << "Pi =" << endl;
+			//Wyswietl(Pi,Pi.size());
+			//cout << endl;
+		}
+		else t = min_r(N);
+		cout << t << " ";
+	}
+	cout << "]";
+	return Pi;
+}
+
+void Kryt(vector<Zdarzenie> wynik)
+{
+	int* S = new int [wynik.size()];
+	int* C = new int [wynik.size()];
+	int C_max, i;
+
+	S[0] = wynik[0].r_j;
+	C[0] = S[0] + wynik[0].p_j;
+	C_max = C[0] + wynik[0].q_j;
+
+	for(i = 1; i<wynik.size(); i++)
+	{
+		S[i] = wynik[i].r_j > C[i-1] ? wynik[i].r_j : C[i-1];
+		C[i] = S[i] + wynik[i].p_j;
+		C_max = C_max > C[i] + wynik[i].q_j ? C_max : C[i] + wynik[i].q_j;
+	}
+
+	cout << "C_max = " << C_max << endl;
+	cout << "indx\tS_j\tC_j\tC_j+q_j" << endl;
+	for(i = 0; i<wynik.size(); i++)
+	{
+		cout << i+1 << "\t" << S[i] << "\t" << C[i] << "\t" << C[i] + wynik[i].q_j << endl;
+	}
+	delete [] S;
+	delete [] C;
 }
