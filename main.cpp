2f8e000ed62029aa86267ee2ce63b0b865e86952
Janusz-Domaradzki <62700201+Janusz-Domaradzki@users.noreply.github.com>
2021-03-21 00:50:39 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,16 +3,29 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <queue>
+//#include <queue>
 
 using namespace std;
 
+enum Tryb {a, b};
 struct Zdarzenie
 {
 	int r_j;
 	int p_j;
 	int q_j;
 	int indx;
+};
+
+struct Element
+{
+	Zdarzenie elem;
+	Element* nast;
+
+	void init(Zdarzenie z1)
+	{
+		elem = z1;
+		nast = nullptr;
+	}
 };
 
 //Funkcja wyswietlajaca zawartosc wektora z zadaniami
@@ -27,11 +40,16 @@ int min_r_idx(vector<Zdarzenie> z1);
 //Funkcja znajdujaca indeks zdarzenia o najwiekszym parametrze q
 int max_q_idx(vector<Zdarzenie> z1);
 
+//Sortowanie Zadan na podstawie najmniejszego r
+void sort_r(vector<Zdarzenie> &z1);
+
 //Algorytm Schrage
-vector<Zdarzenie> Schrage_alg1(vector<Zdarzenie> z1);
+vector<Zdarzenie> Schrage_alg1(vector<Zdarzenie> z1, Tryb tt);
 
 //Kryterium optymalizacyjne, czyli czas zakonczenia wszystkich zadan i opuszczenia ich przez system
 void Kryt(vector<Zdarzenie> wynik);
+
+
 
 int main()
 {
@@ -79,22 +97,34 @@ int main()
 		quest2.push_back(quest[i]);
 	}
 	delete [] quest;
+
 	cout << "Przed algorytmem: " << endl;
 	Wyswietl(quest2);
 	Kryt(quest2);
-	wynik=Schrage_alg1(quest2);
+	
+	wynik=Schrage_alg1(quest2, a);
 	cout << endl;
 	cout << "Po algorytmie Schrage 1: " << endl;
 	Wyswietl(wynik);
+	Kryt(wynik);
 
+	/*sort_r(quest2);
+	cout << "Przed algorytmem: " << endl;
+	Wyswietl(quest2);
+	Kryt(quest2);*/
+
+	wynik=Schrage_alg1(quest2, b);
+	cout << endl;
+	cout << "Po algorytmie Schrage 2: " << endl;
+	Wyswietl(wynik);
 	Kryt(wynik);
 
 	return 0;
 }
 
+//WSZYSTKIE FUNKCJE
 
-
-
+//Funkcja wyswietlajaca zawartosc wektora z zadaniami
 void Wyswietl(vector<Zdarzenie> z1)
 {
 	cout << "j = [ ";
@@ -126,6 +156,7 @@ void Wyswietl(vector<Zdarzenie> z1)
 	cout << "]" << endl;
 }
 
+//Funkcja znajdujaca Zdarzenie z najmniejszym parametrem r
 int min_r(vector<Zdarzenie> z1)
 {
 	int mm=z1[0].r_j;
@@ -143,6 +174,7 @@ int min_r(vector<Zdarzenie> z1)
 	return mm;
 }
 
+//Funkcja znajdujaca indeks zdarzenia o najmniejszym parametrze r
 int min_r_idx(vector<Zdarzenie> z1)
 {
 	int mm=z1[0].r_j;
@@ -160,6 +192,7 @@ int min_r_idx(vector<Zdarzenie> z1)
 	return id;
 }
 
+//Funkcja znajdujaca indeks zdarzenia o najwiekszym parametrze q
 int max_q_idx(vector<Zdarzenie> z1)
 {
 	int mm=z1[0].q_j;
@@ -177,17 +210,37 @@ int max_q_idx(vector<Zdarzenie> z1)
 	return id;
 }
 
-vector<Zdarzenie> Schrage_alg1(vector<Zdarzenie> z1)
+void sort_r(vector<Zdarzenie> &z1)
+{
+	for(int i=0; i<z1.size(); i++)
+	{
+		for(int j = 0; j<z1.size(); j++)
+		if(z1[j].r_j>z1[i].r_j)
+		{
+			swap(z1[j],z1[i]);
+		}
+	}
+}
+
+vector<Zdarzenie> Schrage_alg1(vector<Zdarzenie> z1, Tryb tt)
 {
 	int k = 0;
 	int new_j;
 	vector <Zdarzenie> G;
-	vector <Zdarzenie> N;
+	vector <Zdarzenie> N = z1;
 	vector <Zdarzenie> Pi;
-	N = z1;
+	switch(tt)
+	{
+		case a:
+		break;
+		case b:
+		sort_r(N);
+		break;
+		default:
+		exit(0);
+	}
 	int t = min_r(N);
-	cout << "t = [ ";
-	cout << t << " ";
+	//cout << "t = [ "<< t << " ";
 	
 	//cout << "??: " <<(int) N.begin() << endl;
 	//cout << G.size() << " " << N.size() << " " << t << endl;
@@ -224,9 +277,9 @@ vector<Zdarzenie> Schrage_alg1(vector<Zdarzenie> z1)
 			//cout << endl;
 		}
 		else t = min_r(N);
-		cout << t << " ";
+		//cout << t << " ";
 	}
-	cout << "]";
+	//cout << "]";
 	return Pi;
 }
 
@@ -247,12 +300,14 @@ void Kryt(vector<Zdarzenie> wynik)
 		C_max = C_max > C[i] + wynik[i].q_j ? C_max : C[i] + wynik[i].q_j;
 	}
 
-	cout << "C_max = " << C_max << endl;
+	cout << endl << "C_max = " << C_max << endl << endl;
 	cout << "indx\tS_j\tC_j\tC_j+q_j" << endl;
 	for(i = 0; i<wynik.size(); i++)
 	{
 		cout << i+1 << "\t" << S[i] << "\t" << C[i] << "\t" << C[i] + wynik[i].q_j << endl;
 	}
+	cout << endl;
 	delete [] S;
 	delete [] C;
 }
+
