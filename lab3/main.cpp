f3e6acf5f1c35daf533068d65b2c2540ec63eed0
Janusz-Domaradzki <62700201+Janusz-Domaradzki@users.noreply.github.com>
2021-05-06 16:23:04 +0200

    Lab3 done

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -64,7 +64,6 @@ int Kryt(vector<Zadanie> z1)
 		C[i] = S[i]+z1[i].p_j;
 		T[i] = max(C[i]-z1[i].d_j, 0);
 		F+=T[i]*z1[i].w_j;
-		//cout << "S= " << S[i] << " C= " << C[i] << " T= " << T[i] << endl;
 	}
 
 	return F;
@@ -78,7 +77,7 @@ vector<Zadanie> Sort_it(vector<Zadanie> z1)
 	{
 		for(int j=0; j<wynik.size(); j++)
 		{
-			if(wynik[j].d_j >= wynik[i].d_j)
+			if(wynik[j].d_j > wynik[i].d_j)
 			{
 				swap(wynik[j], wynik[i]);
 			}
@@ -87,8 +86,33 @@ vector<Zadanie> Sort_it(vector<Zadanie> z1)
 	return wynik;
 }
 
+
+
+void BruteForce1(vector<Zadanie> z1, int iteracja, int &kryt1) 
+{
+	
+	
+    if (iteracja == z1.size()) 
+    {
+        if(Kryt(z1) < kryt1){
+        	kryt1 = Kryt(z1);
+        	cout << kryt1 << endl;
+		}	
+        return;
+    }
+
+    for (int i = iteracja; i <= z1.size() - 1; i++)
+    {
+
+        swap(z1[iteracja], z1[i]);
+        BruteForce1(z1, iteracja + 1,kryt1);
+        swap(z1[iteracja], z1[i]);
+    }
+}
+
 int main()
 {
+	//string znaki = "efsfsa";
 	long ziarno;
 	cout << "Podaj ziarno: ";
 	cin >> ziarno;
@@ -102,7 +126,7 @@ int main()
 	Zadanie* quest = new Zadanie [wlk];
 
 	vector<Zadanie> quest2;
-	vector<Zadanie> wynik;
+	vector<Zadanie> wynik1;
 	int i, X;
 	int A = 0;
 	char x;
@@ -141,7 +165,12 @@ int main()
 
 	delete [] quest ;
 	Wyswietl(quest2);
-	//Wyswietl(Sort_it(quest2));
+
+	wynik1 = quest2;
+	cout << endl;
+	int test111 = Kryt(wynik1); 
+	BruteForce1(wynik1, 0, test111);
+
 	cout << "neutral WiTi sum = " << Kryt(quest2) << endl;
 	cout << "greedy WiTi sum = " << Kryt(Sort_it(quest2)) << endl;
 	return 0;
